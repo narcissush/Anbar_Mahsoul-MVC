69212e381eccbaaa89ec4f101cda0eda0338c0a8
hajizadeh <narges.hajizadeh65@gmail.com>
2025-06-25 19:48:15 +0330

    validation

--- src/main/java/anbar/controller/validation/UserValidation.java
+++ src/main/java/anbar/controller/validation/UserValidation.java
@@ -11,13 +11,20 @@ public class UserValidation {
             Pattern.compile("^[A-Za-z]\\w{5,29}$");
 
     private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile(
+                    "^(?=.*[0-9])" +
+                            "(?=.*[a-z])" +
+                            "(?=.*[A-Z])" +
+                            "(?=.*[@#$%^&+=!])" +
+                            "(?=\\S+$).{8,20}$"
+            );
+
+    private static final Pattern NATIONALID_PATTERN =
             Pattern.compile("^[A-Za-z]\\w{5,29}$");
 
     public static boolean isValid(String username) {
         if (username == null) return false;
         return USERNAME_PATTERN.matcher(username).matches();
     }
-
-
 }
 
